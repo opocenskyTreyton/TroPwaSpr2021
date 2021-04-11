2d1bbd366e8f4957fec784dd7afc8aeaa860d590
opocenskyTreyton <54596181+opocenskyTreyton@users.noreply.github.com>
2021-04-11 16:01:42 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// a list of local resources we always want to be cached
+const PRECACHE_URLS = [
+  'index.html',
+  './', //Alias for index.html
+  'css/style.css',
+  'js/scriptsW.js',
+  'sw.js'
+  ];
+
+// the install handler takes care of precaching our resources as directed
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(PRECACHE).then(function(cache){
+      return cache.addAll(PRECACHE_URLS);
+    })
+  );
+});
+
+// the activate handler
+self.addEventListener('activate', event => {
+  console.log('Service worker activating . . .');
+});
+
+// the fetch handler
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
+});
